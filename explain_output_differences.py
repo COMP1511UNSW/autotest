d9649cc80b7297a094218f91b5038d7e9de0ed69
abiramen <7523422+abiramen@users.noreply.github.com>
2021-10-20 00:15:54 +1100

    Move to using f-strings as appropriate

--- explain_output_differences.py
+++ explain_output_differences.py
@@ -12,30 +12,30 @@ def explain_output_differences(name, expected, canonical_expected, canonical_exp
 	max_line_length_shown = int(max_line_length_shown)
 	colored = termcolor_colored if parameters['colorize_output'] else lambda x,*a,**kw: x
 	if canonical_expected and not actual:
-		return colored("Your program produced no output\n", 'red')
-	if debug: print("explain_output_differences(%s, '%s', '%s')" % (name, expected, actual))
+		return colored('Your program produced no output\n', 'red')
+	if debug: print(f'explain_output_differences({name}, {expected}, {actual})')
 
 	# check if output is correct but just missing a character
 	# but don't use this for short outputs
-	if canonical_expected[0:-1] == canonical_actual and (len(canonical_actual) > 8 or (canonical_actual and canonical_actual[-1] in ". \n")):
+	if canonical_expected[0:-1] == canonical_actual and (len(canonical_actual) > 8 or (canonical_actual and canonical_actual[-1] in '. \n')):
 		missing_char = colored(repr(canonical_expected[-1]), 'red')
-		return "Your program's %s was correct except it was missing a %s character on the end.\n" % (name, missing_char)
+		return f'Your program\'s {name} was correct except it was missing a {missing_char} character on the end.\n'
 
 	# check if output is correct but has an extra character
 	# but don't use this for short outputs
 
 	# there is a special case where an extra '\n' will cause trailing spaces on the last line of expected output
 	# to be stripped actual[0:-1] == canonical_expected remedies that in some cases
-	if ((canonical_actual[0:-1] == canonical_expected or actual[0:-1] == canonical_expected) and
-		(len(canonical_actual) > 8 or (canonical_actual and canonical_actual[-1] in ". \n"))):
+	if ((canonical_actual[:-1] == canonical_expected or actual[:-1] == canonical_expected) and
+		(len(canonical_actual) > 8 or (canonical_actual and canonical_actual[-1] in '. \n'))):
 		extra_char = colored(repr(canonical_actual[-1]), 'red')
 		suffix = ''
 		if '\ufffd' == canonical_actual[-1]:
-			extra_char = colored("\'\\xff'", 'red')
+			extra_char = colored('\'\\xff\'', 'red')
 			suffix = 'This can result from printing the EOF value returned by getchar.\n'
-		return "Your program's %s was correct except it had an extra %s character on the end.\n%s" % (name, extra_char,suffix)
-	actual_line_color = defaultdict(lambda:'green')
-	explanation = ""
+		return f'Your program\'s {name} was correct except it had an extra {expected} character on the end.\n{suffix}'
+	actual_line_color = defaultdict(lambda: 'green')
+	explanation = ''
 	actual_lines = actual.splitlines()
 	n_actual_lines = len(actual_lines)
 	expected_lines = expected.splitlines()
@@ -44,34 +44,34 @@ def explain_output_differences(name, expected, canonical_expected, canonical_exp
 	n_canonical_expected_lines = len(canonical_expected_lines)
 
 	if n_actual_lines > 1:
-		actual_description = "Your program produced these %d lines of %s:\n" % (n_actual_lines, name)
+		actual_description = f'Your program produced these {n_actual_lines} lines of {name}:\n'
 	else:
-		actual_description = "Your program produced this line of %s:\n" % (name)
+		actual_description = 'Your program produced this line of {name}:\n'
 		
 	if not expected and actual:
 		explanation = colored(
-			"No {0} was expected for this test and your program produced {1} lines of {0}\n".format(name, n_actual_lines)
-			, 'red')
+			f'No {name} was expected for this test and your program produced {n_actual_lines} lines of {name}\n',
+			 'red')
 		if show_actual_output:
 			explanation += actual_description
-			explanation += sanitize_string(actual, line_color= defaultdict(lambda:'red' if parameters['colorize_output'] else ''), **parameters)
+			explanation += sanitize_string(actual, line_color = defaultdict(lambda: 'red' if parameters['colorize_output'] else ''), **parameters)
 		return explanation
 	if len(canonical_expected_lines) == len(canonical_actual_lines) and 2 < len(canonical_actual_lines) < 1000 and sorted(canonical_expected_lines) == sorted(canonical_actual_lines):
-		explanation += colored("\nYour program produced the correct %s lines but in the wrong order.\n" % name, 'red')
+		explanation += colored('\nYour program produced the correct {name} lines but in the wrong order.\n', 'red')
 
 	# FIXME changes  canonical_actual_lines, canonical_expected_lines, actual_lines, expected_lines
 	diff_explanation = create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, expected_lines, name, colored, max_lines_shown, debug, actual_line_color)
 	if not parameters['colorize_output']:
-		actual_line_color = defaultdict(lambda:'')
+		actual_line_color = defaultdict(lambda: '')
 
 	if show_actual_output:
 		if actual_lines:
 			explanation += actual_description
 			explanation += sanitize_string(actual, line_color=actual_line_color, **parameters)
 		if expected and actual and actual[-1] != '\n' and expected[-1] == '\n':
-			 explanation += "Last line of output above was not terminated with a newline('\\n') character\n"
+			explanation += 'Last line of output above was not terminated with a newline(\'\\n\') character\n'
 		if show_expected_output and canonical_expected_lines:
-			explanation += "\nThe correct %d lines of %s for this test were:\n" % (n_canonical_expected_lines, name)
+			explanation += f'\nThe correct {n_canonical_expected_lines} lines of {name} for this test were:\n'
 			explanation += colored(sanitize_string(expected, **parameters), 'green')
 
 
@@ -79,7 +79,7 @@ def explain_output_differences(name, expected, canonical_expected, canonical_exp
 		return explanation
 
 	# check if the difference is a simple character transliteration
-	if 8 < len(canonical_expected)  < 1000:
+	if 8 < len(canonical_expected) < 1000:
 		actual_chars = set(canonical_actual)
 		expected_chars = set(canonical_expected)
 		actual_not_expected = actual_chars - expected_chars
@@ -87,30 +87,31 @@ def explain_output_differences(name, expected, canonical_expected, canonical_exp
 		if len(actual_not_expected) == 1 and len(expected_not_actual) == 1:
 			actual_char = actual_not_expected.pop()
 			expected_char = expected_not_actual.pop()
+
 			if canonical_actual.replace(actual_char, expected_char) == canonical_expected:
-				n = canonical_actual.count(actual_char)
-				if n == 1:
-					format = "a '%s' with a '%s' character."
-				else:
-					format = "all '%s' characters with '%s' characters."
-				explanation += "\nYour program's %s would be correct if you replaced " % name
-				explanation += format % (colored(actual_char, 'red'), colored(expected_char, 'green'))
-				explanation += "\n"
+				n_mismatch = canonical_actual.count(actual_char)
+				fmt = 'all \'%s\' characters with \'%s\' characters.'
+				if n_mismatch == 1:
+					fmt = 'a \'%s\' with a \'%s\' character.' 'all \'%s\' characters with \'%s\' characters.'
+
+				explanation += '\nYour program\'s {output} would be correct if you replaced '
+				explanation += fmt % (colored(actual_char, 'red'), colored(expected_char, 'green'))
+				explanation += '\n'
 				return explanation
+
 		if len(actual_not_expected) == 1 and len(expected_not_actual) == 0:
 			actual_char = actual_not_expected.pop()
 			if canonical_actual.replace(actual_char, '') == canonical_expected:
-				n = canonical_actual.count(actual_char)
-				if n == 1:
-					format = "a '%s' character."
-				else:
-					format = "all '%s' characters."
-				explanation += "Your program's %s would be correct if you removed " % name
-				explanation += format % (colored(actual_char, 'red'))
-				explanation += "\n"
+				n_mismatch = canonical_actual.count(actual_char)
+				fmt = 'all \'%s\' characters.'
+				if n_mismatch == 1:
+					fmt = 'a \'%s\' character.'
+				explanation += f'Your program\'s {name} would be correct if you removed '
+				explanation += fmt % (colored(actual_char, 'red'))
+				explanation += '\n'
 				return explanation
 	if diff_explanation:
-		explanation +=  "\n" + "\n".join(diff_explanation[0:max_lines_shown]) + "\n"
+		explanation +=  '\n' + '\n'.join(diff_explanation[:max_lines_shown]) + '\n'
 	return explanation
 
 def create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, expected_lines, name, colored, max_lines_shown, debug, actual_line_color):
@@ -162,7 +163,9 @@ def create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, 
 	expected_line_number = 0
 	actual_line_number = 0
 	diff = difflib.ndiff(canonical_actual_lines, canonical_expected_lines)
-	diff_explanation = ["The difference between your %s(%s) and the correct %s(%s) is:" % (name, colored("-", 'red'), name, colored("+", 'green'))]
+	diff_explanation = [
+		f'The difference between your {name}({colored("-", "red")}) and the correct {name}({colored("+", "green")}) is:'
+	]
 	if prefix_removed:
 		diff_explanation.append('...')
 	try:
@@ -175,17 +178,17 @@ def create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, 
 				continue
 			last_d = d
 			d = diff_line[0]
-			if d in "-+" and diff_explanation and context_line and diff_explanation[-1] == '...':
+			if d in '-+' and diff_explanation and context_line and diff_explanation[-1] == '...':
 				diff_explanation[-1] = context_line
 			context_line = None
 			if d == '-':
 				diff_explanation.append(colored('- ' + actual_lines[actual_line_number], 'red'))
 				actual_line_color[prefix_removed+actual_line_number] = 'red'
 				actual_line_number += 1
-			elif d == "+":
+			elif d == '+':
 				diff_explanation.append(colored('+ ' + expected_lines[expected_line_number], 'green'))
 				expected_line_number += 1
-			elif d == "?":
+			elif d == '?':
 				# include column marker only if line has not been changed before diff
 				if last_d == '+':
 					if canonical_expected_lines[expected_line_number - 1] == expected_lines[expected_line_number - 1]:
@@ -193,7 +196,7 @@ def create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, 
 				elif last_d == '-':
 					if canonical_actual_lines[actual_line_number - 1] == actual_lines[actual_line_number - 1]:
 						diff_explanation.append(diff_line)
-			elif d == " ":
+			elif d == ' ':
 				context_line = '  ' + actual_lines[actual_line_number]
 				if last_line_in_diff:
 					diff_explanation.append(context_line)
@@ -202,7 +205,7 @@ def create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, 
 					dotdotdot_added = True
 				actual_line_number += 1
 				expected_line_number += 1
-			if d in "-+":
+			if d in '-+':
 				dotdotdot_added = False
 				last_line_in_diff = True
 			else:
@@ -212,7 +215,7 @@ def create_diff(canonical_actual_lines, canonical_expected_lines, actual_lines, 
 		if (diff_truncated or suffix_removed) and diff_explanation[-1] != '...':
 			diff_explanation.append('...')
 	except IndexError:
-		if debug: print("IndexError: unexpected diff output")
+		if debug: print('IndexError: unexpected diff output')
 		# unexpected diff output might break above code
 		pass
 	return diff_explanation
