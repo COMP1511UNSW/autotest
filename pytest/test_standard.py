e6a7d72bf29c35e485f85765ed98d16b16470c5a
hexDoor <hexDoor@users.noreply.github.com>
2021-10-24 05:27:15 +0000

    auto - fixup python code with psf/black

--- pytest/test_standard.py
+++ pytest/test_standard.py
@@ -4,7 +4,6 @@ import re
 
 # created from at's original test script (yes this script needs to be thrown into a fire)
 class TestStandard:
-
     def test_arguments(self):
         test_folder = "../tests/arguments"
         p = subprocess.run(
@@ -18,7 +17,6 @@ class TestStandard:
             print(p.stdout)
             assert False
 
-
     # this one is a top level one (this is broken because glob things)
     def test_checker(self):
         return
@@ -27,7 +25,8 @@ class TestStandard:
             args=["../autotest.py", "-D", test_folder, "-a", test_folder],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
-            timeout=10, encoding="utf-8",
+            timeout=10,
+            encoding="utf-8",
         )
         if not re.sedarch(" tests passed 0 tests failed *$", p.stdout):
             print(p.stdout)
@@ -36,7 +35,9 @@ class TestStandard:
     def test_environment(self):
 
         test_folder = "../tests/environment"
-        test_env = {"SAMPLE_ENVIRONMENT_VARIABLE": "sample_value"} # this is cursed but it's necessary
+        test_env = {
+            "SAMPLE_ENVIRONMENT_VARIABLE": "sample_value"
+        }  # this is cursed but it's necessary
         p = subprocess.run(
             args=["../autotest.py", "-D", test_folder, "-a", f"{test_folder}/autotest"],
             env=test_env,
