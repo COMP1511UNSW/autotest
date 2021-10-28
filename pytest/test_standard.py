3df8a00df4f08781c7d1ff09366090a52d691259
sschreyer <samschreyer360@gmail.com>
2021-10-28 23:54:59 +1100

    Added f-strings and ignore tests to test_standard.py

--- pytest/test_standard.py
+++ pytest/test_standard.py
@@ -49,3 +49,44 @@ class TestStandard:
         if not re.search(" tests passed 0 tests failed *$", p.stdout):
             print(p.stdout)
             assert False
+
+    def test_expected_output(self):
+        test_folder = "../tests/expected_output"
+        p = subprocess.run(
+            args=["../autotest.py", "-D", test_folder, "-a", f"{test_folder}/autotest"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            timeout=10,
+            encoding="utf-8",
+        )
+        if not re.search(" tests passed 0 tests failed *$", p.stdout):
+            print(p.stdout)
+            assert False
+
+    def test_f_strings(self):
+        test_folder = "../tests/f-strings"
+        p = subprocess.run(
+            args=["../autotest.py", "-D", test_folder, "-a", f"{test_folder}/autotest"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            timeout=10,
+            encoding="utf-8",
+        )
+        if not re.search(" tests passed 0 tests failed *$", p.stdout):
+            print(p.stdout)
+            assert False
+
+    def test_ignore(self):
+        test_folder = "../tests/ignore"
+        p = subprocess.run(
+            args=["../autotest.py", "-D", test_folder, "-a", f"{test_folder}/autotest"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            timeout=10,
+            encoding="utf-8",
+        )
+        if not re.search(" tests passed 0 tests failed *$", p.stdout):
+            print(p.stdout)
+            assert False
+
+    
