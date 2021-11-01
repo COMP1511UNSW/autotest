e274b16d85f73b8af1612455a075065cd1bc9d81
abiramen <7523422+abiramen@users.noreply.github.com>
2021-11-01 17:01:28 +1100

    Fix erroneous s/input/std_input/g

--- run_test.py
+++ run_test.py
@@ -283,7 +283,7 @@ class _Test:
         n_input_lines = std_input.count("\n")
         if self.parameters["show_stdin"]:
             if std_input and n_input_lines < 32:
-                self.long_explanation += f"\nThe std_input for this test was:\n{colored(std_input, 'yellow')}\n"
+                self.long_explanation += f"\nThe input for this test was:\n{colored(std_input, 'yellow')}\n"
                 if std_input[-1] != "\n" and "\n" in std_input[:-2]:
                     self.long_explanation += (
                         "Note: last character in above input is not '\\n'\n\n"
